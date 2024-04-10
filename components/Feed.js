9c27baff7e86a5cb76cc2c6434cb42b9b8856dee
Robin Khuc <robin.khuc@beyondedge.co>
2024-04-10 17:25:08 +0700

    add the post section of the feed component

--- components/Feed.js
+++ components/Feed.js
@@ -1,5 +1,29 @@
 import { SparklesIcon } from '@heroicons/react/24/outline';
 import Input from './Input';
+import Post from './Post';
+
+const posts = [
+  {
+    id: 1,
+    name: 'Robert Khuc',
+    username: 'ktphuc1994',
+    userImg:
+      'https://media.licdn.com/dms/image/C5603AQEeDVn0pnKpDQ/profile-displayphoto-shrink_800_800/0/1623814405277?e=2147483647&v=beta&t=6KsthbGW6_lqQg_GVW7rjiaeHk9rs4-uJx1eN_4OYtg',
+    img: 'https://images.unsplash.com/photo-1470071459604-3b5ec3a7fe05?q=80&w=2548&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    text: 'Nice view!',
+    timestamp: '2 hours ago',
+  },
+  {
+    id: 2,
+    name: 'Robert Khuc',
+    username: 'ktphuc1994',
+    userImg:
+      'https://media.licdn.com/dms/image/C5603AQEeDVn0pnKpDQ/profile-displayphoto-shrink_800_800/0/1623814405277?e=2147483647&v=beta&t=6KsthbGW6_lqQg_GVW7rjiaeHk9rs4-uJx1eN_4OYtg',
+    img: 'https://images.unsplash.com/photo-1586348943529-beaae6c28db9?q=80&w=2515&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    text: 'WWoww!',
+    timestamp: '2 days ago',
+  },
+];
 
 export default function Feed() {
   return (
@@ -11,6 +35,9 @@ export default function Feed() {
         </div>
       </div>
       <Input />
+      {posts.map((post) => (
+        <Post key={post.id} post={post} />
+      ))}
     </div>
   );
 }
